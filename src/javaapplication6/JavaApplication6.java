732a468fec1e4c78622c198dd641349879d76902
Wind <Wind@10.5.12.234>
2018-01-22 20:58:15 +0700

    FinishTe

--- src/javaapplication6/JavaApplication6.java
+++ src/javaapplication6/JavaApplication6.java
@@ -16,7 +16,7 @@ public class JavaApplication6 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello Test!");
+        System.out.println("Hello Test!!!");
     }
     
 }
